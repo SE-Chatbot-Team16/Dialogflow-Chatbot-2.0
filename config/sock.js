9557ae84a0561575eabd7885df2eb7f28560af0d
k0nkupa <theton369@gmail.com>
2018-05-27 22:29:07 +1200

    Refactor code for cleaner and more optimise

--- config/sock.js
+++ config/sock.js
@@ -4,7 +4,7 @@ var io = require('socket.io').listen(server);
 var db = require('./connection');
 var api = require('./api');
 var mongoose = require('mongoose');
-var preco
+var preco, isPaper;
 //return from server and pass to botui
 var fromClient = function(socket) {
     socket.on('fromClient', function (data) {
@@ -29,142 +29,109 @@ var fromClient = function(socket) {
             } else {
               console.log('error');
             }//end if
-          }else if (res.result.parameters.Requisites == 'pre') { //Get Pre
-            preco = true;
-            getRequisite(res,socket);
-          }else if (res.result.parameters.Requisites == 'co') { //Get Co
-            preco = false
-            getRequisite(res,socket);
-          }else if (res.result.parameters.Year || res.result.parameters.Semester || res.result.parameters.Location) {
-            getYearSemesterLocation(res, socket);
-          }else if (res.result.parameters.YearStudy && res.result.contexts[0].parameters.Paper) {
-            var paper = res.result.contexts[0].parameters.Paper;
-            var yearstudy = res.result.contexts[0].parameters.YearStudy;
-            console.log(res.result);
-            getFailPaper(res, socket, paper, yearstudy);
-          }else {
+          }
+          //Loop through all paper
+          db.find({})
+          .exec()
+          .then(docs => {
+            for(let i in docs) {
+              if(res.result.parameters.Paper == docs[i].name) {
+                if(res.result.parameters.Requisites == 'pre') {
+                  preco = true;
+                  getRequisite(res,socket, docs[i]);
+                }else if (res.result.parameters.Requisites == 'co') { //Get Co
+                  preco = false
+                  getRequisite(res,socket, docs[i]);
+                }else if (res.result.parameters.Year || res.result.parameters.Semester || res.result.parameters.Location) {
+                  getYearSemesterLocation(res, socket, docs[i]);
+                }else if (res.result.metadata.intentName == "FailPaper") {
+                  getFailPaper(res, socket, docs[i]);
+                }else if (res.result.parameters.recommendPaper) {
+                  getRecommendPaper(res, socket, docs[i]);
+                }
+              }//end if 1
+              else {
+                isPaper = false;
+              }
+            }//end for
+          }).catch(err => {console.log(err);});
+          if(!isPaper){
             socket.emit('fromServer', { server: res.result.fulfillment.speech });
           }
-          // end if
+
       });//end api.getRes()
     });//end socket.on
 };//end function
 
-//get Requisites
-var getRequisite = function (res,socket) {
+//Get Pre and Co requisites
+var getRequisite = function (res,socket, docs) {
   var bln =true;
-  //loop through the file
-  db.find({})
-  .exec()
-  .then(docs => {
-    for(let i in docs){
-      //check if the paper is in the system or not
-      if(res.result.parameters.Paper === docs[i].name) {
-        if(preco) {
-          var result = res.result.fulfillment.speech + " " + docs[i].pre;
-        }else {
-          var result = res.result.fulfillment.speech + " " + docs[i].co;
-        }
-        socket.emit('fromServer', { server: result });
-        bln = true;
-        break;
-      }else {
-        bln = false;
-      }//end if
-    }// end for
-    if(!bln) {
-      var fault = "The paper you are looking for is not in the system";
-      socket.emit('fromServer', { server: fault });
-    }
-  })//end then
-  .catch(err => {console.log(err);});
+  if(preco) {
+    var result = res.result.fulfillment.speech + " " + docs.pre;
+  }else {
+    var result = res.result.fulfillment.speech + " " + docs.co;
+  }
+  socket.emit('fromServer', { server: result });
 };
 
-var getYearSemesterLocation = function (res, socket) {
-  var bln = true;
-  db.find({})
-  .exec()
-  .then(docs => {
-    for(let i in docs) {
-      if(res.result.parameters.Paper === docs[i].name) {
-        if(res.result.parameters.Year){
-          var result = res.result.fulfillment.speech + " " + docs[i].year;
-        }else if (res.result.parameters.Semester) {
-          var result = res.result.fulfillment.speech + " " + docs[i].semester;
-        }else if (res.result.parameters.Location) {
-          var result = res.result.fulfillment.speech + " " + docs[i].location;
-        }
-        socket.emit('fromServer', { server: result });
-        bln = true;
-        break;
-      }else {
-        bln = false;
-      }
-    }//end for
-    if(!bln) {
-      var fault = "The paper you are looking for is not in the system";
-      socket.emit('fromServer', { server: fault });
-    }//end if
-  }).catch(err => {console.log(err);});
+//Get year, semester, location
+var getYearSemesterLocation = function (res, socket,docs) {
+  if(res.result.parameters.Year){ //Year
+    var result = res.result.fulfillment.speech + " " + docs.year;
+  }else if (res.result.parameters.Semester) {//Semester
+    var result = res.result.fulfillment.speech + " " + docs.semester;
+  }else if (res.result.parameters.Location) {//Location
+    var result = res.result.fulfillment.speech + " " + docs.location;
+  }
+  socket.emit('fromServer', { server: result });
 };
 
-/*
-Get paper that can't take when fail <a specific paper>
-@params
-bln : if the paper is not in the system
-code : paper code of the failed paper
-pre : get pre-requisites of the current paper in the for loop
-resultyes : paper can take
-resultno : paper cannot take
-*/
-var getFailPaper = function(res, socket, paper, yearstudy) {
-  var bln = true;
-  var code, pre, isCore;
-  var result = " ";
-  var j = 0;
+//Get fail paper
+var getFailPaper = function(res, socket, docs) {
+  //check paper isCore
+  if(docs.core == 'core') {
+    result = ", You have to redo " + res.result.parameters.Paper + " because it's a core paper ";
+  }else {
+    result = ", You can take other 15 points from elective paper from the current year of study ";
+  }
+  //get code of the fail paper
+  code = docs.code;
+  //loop through all the papers
+  var result__ , result_;
   db.find({})
   .exec()
-  .then(docs => {
-    for(let i in docs){
-      /* Check if the paper is in the system or not
-      ---------------------------------------------------*/
-      if(paper === docs[i].name) {
-        console.log('2');
-        //check paper isCore
-        if(docs[i].core == 'core') {
-          console.log('1');
-          isCore = true;
-          result = " You have to redo " + paper + " because it's a core paper ";
-        }else {
-          isCore = false;
-          result = " ";
-        }
-        code = docs[i].code;
-        bln = true;
-      }
-      /* If pre-req of current paper contain paper failed
-      ---------------------------------------------------*/
-      pre = docs[i].pre;
+  .then(lines => {
+    var result_ = ", You can't take these paper: ";
+    var bln;
+    for(let i in lines) {
+      var pre = lines[i].pre;
       if(pre.includes(code)) {
-          result += "You can't take these paper: [" +docs[i].code + " " + docs[i].name + "], ";
-      }else {
-        j++;
+        bln = true;
+        result_ += "[ " + lines[i].code + " " + lines[i].name + " ] ";
       }
-      //Paper that is not pre-req of any papers
-      if(isCore == false) {
-        if(j == docs.length) {
-          result = " Other 15 points from elective paper from the current year of study ";
-        }
-      }
-    }// end for
-
-    // Print out result
-    socket.emit('fromServer', { server: res.result.fulfillment.speech + result });
-    if(!bln) {
-      var fault = "The paper you are looking for is not in the system";
-      socket.emit('fromServer', { server: fault });
-    }//end if
-  })//end then
-  .catch(err => {console.log(err);});
+    }
+    if(bln) {
+      socket.emit('fromServer', { server: res.result.fulfillment.speech + result + result_});
+    }else {
+      socket.emit('fromServer', { server: res.result.fulfillment.speech + result });
+    }
+  }).catch(err => {console.log(err);});
 }
+
+//Get Recommend Paper
+var getRecommendPaper = function(res, socket, docs) {
+  var yearstudy = docs.yearstudy;
+  var result = "";
+  db.find({})
+  .exec()
+  .then(lines => {
+    for(let i in lines) {
+      if(lines[i].yearstudy == yearstudy) {
+        result += " [" + lines[i].code + " " + lines[i].name + "] ";
+      }
+    }
+    socket.emit('fromServer', { server: res.result.fulfillment.speech + result });
+  }).catch(err => {console.log(err);});
+}
+
 module.exports = {fromClient}
